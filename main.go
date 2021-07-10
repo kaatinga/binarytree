ce41f490cdb7e734aed77a7e82e5bd7d1c47a916
Kaatinga <painter@3lines.club>
2021-07-10 20:32:55 +0300

    test run with rand package

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
+import "math/rand"
+
 func main() {
 
 	tree := Tree{}
 
-	tree.testValue(128, "test128")
-	tree.testValue(12, "test12")
-	tree.testValue(60, "test60")
-	tree.testValue(13, "test13")
-	tree.testValue(6, "test6")
-	tree.testValue(121, "test121")
-	tree.testValue(129, "test129")
-	tree.testValue(129, "test129new")
+	tree.testValue(128)
+
+	var number32, i int32
+	for i < 600 {
+		number32 = rand.Int31n(255)
+		tree.testValue(byte(number32))
+		i++
+	}
 }
