3a551d1177a020c374393ac9679a22de5a932cc3
Kaatinga <painter@3lines.club>
2021-07-10 17:13:37 +0300

    additional test calls

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ func main() {
 
 	tree := Tree{}
 
-	tree.Insert(128, "test")
+	tree.Insert(128, "test128")
 	fmt.Println(tree.Find(128))
+
+	tree.Insert(127, "test127")
+	fmt.Println(tree.Find(127))
+
+	tree.Insert(126, "test126")
+	fmt.Println(tree.Find(126))
 }
