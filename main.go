25ec6710f1984c500e78c21c04e6a7172409ca51
Kaatinga <painter@3lines.club>
2021-07-10 18:01:55 +0300

    more items in the test run

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ func main() {
 	tree := Tree{}
 
 	tree.testValue(128, "test128")
-	tree.testValue(122, "test122")
+	tree.testValue(12, "test12")
+	tree.testValue(60, "test60")
+	tree.testValue(13, "test13")
+	tree.testValue(6, "test6")
 	tree.testValue(121, "test121")
 	tree.testValue(129, "test129")
+	tree.testValue(129, "test129new")
 }
